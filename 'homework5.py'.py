b3568ba87822a6a4bb10040e9e42259c0578bf0e
Vitalya35 <165400524+Vitalya35@users.noreply.github.com>
2024-04-06 17:07:19 +0400

    Add files via upload

---
+++ 'homework5.py'.py
@@ -0,0 +1,13 @@
+my_list = ['banana', 'papaya', 'mango', 'peach', 'apple', 'cherry']
+print(my_list)
+print(my_list[0:6:5])
+print(my_list[2:5])
+my_list[2] = 'pitahaya'
+print(my_list)
+my_dict = {'banana':'банан','papaya':'папайя','pitahaya':'питахайя'}
+print(my_dict)
+print(my_dict.get('papaya'))
+my_dict['pitahaya'] = 'драконий фрукт'
+print(my_dict)
+my_dict['apple'] = 'яблоко'
+print(my_dict)
